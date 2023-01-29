3bddbc3ad312a20149d134a13d5303cc0015007a
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-29 16:38:55 +0000

    add capitalisation for the cities' first letter

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,15 @@ function getWeather(city) {
         let iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
         let todayHeading = `${city} ${date}`;
         $("#today").html(
-            `<h2>${todayHeading}<img src= ${iconUrl} alt= "weather icon"}></h2>
+            `<h2 class=today-heading>${todayHeading}<img src= ${iconUrl} alt= "weather icon"}></h2>
         <p>Temperature: ${todayTemp} °C</p>
         <p>Humidity: ${todayHumidity}%</p>
         <p>Wind Speed: ${todayWind} KPH</p>`);
         let forecastDate = date;
         let temp, humidity, forecastIcon, wind, forecastIconUrl;
+        let forecastHeading = $("<h4>5-Day Forecast</h4>").addClass("forecast-heading m-2");
+        let cardsContainer = $("<div>").addClass("row card-group m-2");
+        $("#forecast").append(forecastHeading, cardsContainer);
         for (var i = 7; i < results.length; i += 8) {
             forecastDate = moment(forecastDate, "DD/MM/YY").add(1, "d").format("DD/MM/YY")
             temp = results[i].main.temp;
@@ -29,23 +32,28 @@ function getWeather(city) {
             wind = results[i].wind.speed;
             forecastIcon = results[i].weather[0].icon;
             forecastIconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+            let forecastHeading = $("<h3>5-Day Forecast</h3>");
             let forecastCard = $("<div>");
-            forecastCard.addClass("card p-3 forecast-card bg-primary m-2")
+            forecastCard.addClass("card p-3 forecast-card bg-primary")
             forecastCard.html(
             `<h3>${forecastDate}</h3>
             <img class = "weather-icon" src= ${iconUrl} alt= "weather icon"}>
             <p>Temperature: ${temp} °C</p>
             <p>Humidity: ${humidity}%</p>
             <p>Wind Speed: ${wind} KPH</p>`);
-            $("#forecast").append(forecastCard);
+            $(cardsContainer).append(forecastCard);
         };
         let newButton = $("<button>")
             .text(city)
             .addClass("bg-light history-button m-2");
-        $("#history").prepend(newButton)
+        $("#history").prepend(newButton);
         let historyList = JSON.parse(localStorage.getItem("cities"));
         if (historyList === null) historyList = [];
-        historyList.push(city);
+        if (historyList.length > 9) {
+            historyList.pop();
+            $("#history button:last-child").remove();
+        }
+        historyList.unshift(city);
         localStorage.setItem("cities", JSON.stringify(historyList));
     })
 };
